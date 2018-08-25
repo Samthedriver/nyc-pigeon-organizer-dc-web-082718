f20d59aa3d60e97b6867f1650aef54d4697d6010
github username <github email address>
2018-08-24 23:18:55 -0400

    Develop methods to re-hash a nested hash.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,47 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  organized_data = Hash.new
+  temp_array = Array.new
+  temp_string = String.new
+  #get names
+  data.each do |key, hash|
+    hash.each do |color_key, array|
+      array.each do |new_key|
+        organized_data[new_key] = {}
+      end
+    end
+  end
+
+  #get
+  organized_data.collect do |name, empty_hash|
+    data.each do |key, hash|
+      #if key == :lives
+      #  organized_data[name][key] = {};
+      #else
+        organized_data[name][key] = [];
+      #end
+    end
+  end
+
+  data.each do |key, hash|
+    if key == :lives
+      hash.each do |color_key, array|
+        puts color_key
+        array.each do |value|
+          temp_array = organized_data[value][key]
+          temp_array.push(color_key)
+          organized_data[value][key] = temp_array
+        end
+      end
+    else
+      hash.each do |color_key, array|
+        array.each do |new_key|
+          temp_array = organized_data[new_key][key]
+          temp_array.push(color_key.to_s)
+          organized_data[new_key][key] = temp_array
+        end
+      end
+    end
+  end
+  organized_data
 end
